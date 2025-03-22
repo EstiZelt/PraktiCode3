b782d92da86192f50fd3d55ae27ca4bc5ed09632
EstiZelt <e0527185161@gmail.com>
2025-03-22 21:06:28 +0200

    הוספת קבצי Docker

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3543bd32e1e8691adfc094abf5653e0ba8b61508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6e7284ef623475c435ea2256b6a353f4a37369")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
