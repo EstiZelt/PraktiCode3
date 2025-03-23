84921a8387fffc4a75f1f142b3407d0dac3a9618
EstiZelt <e0527185161@gmail.com>
2025-03-23 03:44:21 +0200

    win3

--- my-express-app/server.js
+++ my-express-app/server.js
@@ -3,44 +3,22 @@ const express = require('express');
 
 // יצירת אפליקציה חדשה של Express
 const app = express();
-const PORT = 10000;
+const PORT = process.env.PORT || 3000;
 
-const getRenderApps = async () => {
-    try {
-      const response = await axios.get(RENDER_API_KEY, {
-        headers: {
-          Authorization: `Bearer ${RENDER_API_URL}`,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching Render services:', error);
-      throw error;
-    }
-  };
-  
-  // Endpoint GET להחזיר את רשימת האפליקציות
-  app.get('/render-apps', async (req, res) => {
-    try {
-      const apps = await getRenderApps();
-      res.json(apps);
-    } catch (error) {
-      res.status(500).json({ message: 'Error fetching Render services' });
-    }
-  });
-// נתיב ברירת מחדל
-// app.get('/', async (req, res) => {
-//     try {
-//         const response = await axios.get(process.env.URL, {
-//             headers: {
-//                 'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
-//             }
-//         });
 
-//         res.json(response.data);
-//     } catch (error) {
-//         res.status(500).json({ error: 'Failed to fetch services', details: error.message });
-//     }});
+//נתיב ברירת מחדל
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(process.env.URL, {
+            headers: {
+                'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch services', details: error.message });
+    }});
 
 // הפעלת השרת
 app.listen(PORT, () => {
