1a3cda2b780d72ad676b1121348205c4da8a8850
EstiZelt <e0527185161@gmail.com>
2025-03-23 03:38:35 +0200

    win2

--- my-express-app/server.js
+++ my-express-app/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // יצירת אפליקציה חדשה של Express
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 10000;
 
 const getRenderApps = async () => {
     try {
